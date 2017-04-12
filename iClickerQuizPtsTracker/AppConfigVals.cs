4528e26a64c6c06d0a2b2c90bcafc72695237991
William H. White <rearviewmirror57@yahoo.com>
2017-04-12 01:00:58 -0400

    Deleted AppConfigVals (I think)

--- iClickerQuizPtsTracker/AppConfigVals.cs
+++ iClickerQuizPtsTracker/AppConfigVals.cs
@@ -1,95 +1,134 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 using System.Configuration;
-using System.Collections.Generic;
+using iClickerQuizPtsTracker.AppExceptions;
 
-public static class AppConfigVals
+namespace iClickerQuizPtsTracker
 {
-    private static byte _xlcolnoRawEmail;
-    private static byte _xlcolnoRawNm;
-    private static byte _xlcolnoRawDataBegins;
-    private static byte _dataNmbrRowLblCols;
-
-    private static string _datahdrID;
-    private static string _datahdrEmail;
-    private static string _datahdrLN;
-    private static string _datahdrFN;
-
-    private static List<String> _badKeys = new List<String>();
-
-	public static AppConfigVals()
-	{
-        ReadAppConfigDataIntoFields();
-    }
-
-    private static void ReadAppConfigDataIntoFields()
+    class AppConfigVals
     {
-        AppSettingsReader ar = new AppSettingsReader();
+        #region fields
+        #region private
+        private static List<String> _badKeys = new List<String>();
+        #endregion
 
-        // Populate fields from App.Config values.  If any exceptions add the 
-        // corrupt key name to _badKeys...
-        try
-        {
-            _xlcolnoRawEmail = (byte)ar.GetValue("ColNoEmailXL", typeof(byte));
-        }
-        catch
-        {
-            _badKeys.Add("ColNoEmailXL");
-        }
+        #region public
+        public static readonly byte ExtFileColNoStudentEmail;
+        public static readonly byte ExtFileColNoStudentName;
+        public static readonly byte ExtFileNmbrRowLblCols;
+        public static readonly byte DataTblNmbrRowLblCols;
 
-        try
-        {
-            _xlcolnoRawNm = (byte)ar.GetValue("ColNoStdntNmXL", typeof(byte));
-        }
-        catch
-        {
-            _badKeys.Add("ColNoStdntNmXL");
-        }
+        public static readonly string DataTblColNoID;
+        public static readonly string DataTblColNoEmail;
+        public static readonly string DataTblColNoLNm;
+        public static readonly string DataTblColNoFNm;
+        #endregion
+        #endregion
 
-        try
+        #region pptys
+        public static List<String> BadAppConfigKeys
         {
-            _dataNmbrRowLblCols =
-                (byte)ar.GetValue("NmbrNonScoreCols", typeof(byte));
+            get
+            {
+                return _badKeys;
+            }
         }
-        catch
-        {
-            _badKeys.Add("NmbrNonScoreCols");
-        }
+        #endregion
 
-        try
+        #region ctor
+        static AppConfigVals()
         {
-            _datahdrID = (string)ar.GetValue("ColID", typeof(string));
-        }
-        catch
-        {
-            _badKeys.Add("ColID");
-        }
+            #region populateReadOnlyFields
+            AppSettingsReader ar = new AppSettingsReader();
 
-        try
-        {
-            _datahdrEmail = (string)ar.GetValue("ColEmail", typeof(string));
-        }
-        catch
-        {
-            _badKeys.Add("ColEmail");
-        }
+            // Populate fields from App.Config values.  If any exceptions add the 
+            // corrupt key name to _badKeys...
+            try
+            {
+                ExtFileColNoStudentEmail = (byte)ar.GetValue("ColNoEmailXL", typeof(byte));
+            }
+            catch
+            {
+                _badKeys.Add("ColNoEmailXL");
+            }
 
-        try
-        {
-            _datahdrFN = (string)ar.GetValue("ColFN", typeof(string));
-        }
-        catch
-        {
-            _badKeys.Add("ColFN");
-        }
+            try
+            {
+                ExtFileColNoStudentName = (byte)ar.GetValue("ColNoStdntNmXL", typeof(byte));
+            }
+            catch
+            {
+                _badKeys.Add("ColNoStdntNmXL");
+            }
 
-        try
-        {
-            _datahdrLN = (string)ar.GetValue("ColLN", typeof(string));
+            try
+            {
+                ExtFileNmbrRowLblCols = (byte)ar.GetValue("NmbrRowLblCols", typeof(Byte));
+            }
+            catch
+            {
+                _badKeys.Add("NmbrRowLblCols");
+            }
+
+            try
+            {
+                DataTblNmbrRowLblCols =
+                    (byte)ar.GetValue("NmbrNonScoreCols", typeof(byte));
+            }
+            catch
+            {
+                _badKeys.Add("NmbrNonScoreCols");
+            }
+
+            try
+            {
+                DataTblColNoID = (string)ar.GetValue("ColID", typeof(string));
+            }
+            catch
+            {
+                _badKeys.Add("ColID");
+            }
+
+            try
+            {
+                DataTblColNoEmail = (string)ar.GetValue("ColEmail", typeof(string));
+            }
+            catch
+            {
+                _badKeys.Add("ColEmail");
+            }
+
+            try
+            {
+                DataTblColNoFNm = (string)ar.GetValue("ColFN", typeof(string));
+            }
+            catch
+            {
+                _badKeys.Add("ColFN");
+            }
+
+            try
+            {
+                DataTblColNoLNm = (string)ar.GetValue("ColLN", typeof(string));
+            }
+            catch
+            {
+                _badKeys.Add("ColLN");
+            }
+#endregion
+
+            // Now check for errors...
+            if(_badKeys.Count > 0)
+            {
+                InalidAppConfigItemException ex = 
+                    new InalidAppConfigItemException();
+                throw ex;
+            }
         }
-        catch
-        {
-            _badKeys.Add("ColLN");
-        }
+        #endregion
     }
 }
